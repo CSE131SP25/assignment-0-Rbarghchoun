2d0b446686f1931f12a919cee3aca24e6a3ba178
rgb646 <rgb646@mst.edu>
2025-01-21 16:44:03 -0600

    Assignment-0 Robot House

--- src/assignment0/RobotInstructions.java
+++ src/assignment0/RobotInstructions.java
@@ -29,11 +29,42 @@ public class RobotInstructions  {
 		 * You will add instructions at the end of the code to cause the support.robot to move
 		 * as you desire.
 		 */
-		robot.forward(60);
-		robot.turnLeft(90);
-		robot.forward(23);
-		robot.turnRight(90);
-		robot.forward(80);
+	
+		 robot.turnLeft(90);
+		 robot.forward(320);
+		 robot.turnRight(90);
+		 robot.forward(200);
+		 robot.turnRight(60);
+		 robot.forward(184);
+		 robot.turnRight(60);
+		 robot.forward(184);
+		 robot.turnRight(60);
+		 robot.forward(200);
+
+		 robot.turnRight(90);
+		 robot.forward(100);
+		 robot.turnRight(90);
+		 robot.forward(100);
+		 robot.turnLeft(90);
+		 robot.forward(50);
+		 robot.turnLeft(90);
+		 robot.forward(100);
+
+		 robot.turnRight(180);
+		 robot.forward(100);
+
+		 robot.turnLeft(90);
+		 robot.forward(100);
+		 robot.turnRight(90);
+		 robot.forward(50);
+		 robot.turnRight(90);
+		 robot.forward(50);
+		 robot.turnRight(90);
+		 robot.forward(50);
+
+
+		 
+
 	}
 
 }
